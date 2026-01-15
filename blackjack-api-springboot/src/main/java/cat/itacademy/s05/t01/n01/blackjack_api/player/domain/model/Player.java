6b69b5727f0480b43cb6901336e441c0b52d41ce
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-15 13:26:22 +0100

    feat: update Player class for betting managing

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/domain/model/Player.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/domain/model/Player.java
@@ -1,10 +1,16 @@
 package cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model;
 
+import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.exception.InsufficientBalanceException;
+import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.valueobject.PlayerBet;
 import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.valueobject.PlayerId;
+import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.valueobject.PlayerName;
+import lombok.Data;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Data
 public class Player {
 
     private PlayerId id;
@@ -13,11 +19,12 @@ public class Player {
     private int totalLosses;
     private int totalGames;
     private double winRate;
+    private BigDecimal balance;
     private final LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
     private Player(PlayerId id, PlayerName name, int totalWins, int totalLosses,
-                   int totalGames, double winRate, LocalDateTime createdAt, LocalDateTime updatedAt) {
+                   int totalGames, double winRate, LocalDateTime createdAt, LocalDateTime updatedAt, BigDecimal balance) {
         this.id = id;
         this.name = Objects.requireNonNull(name, "Player name cannot be null");
         this.totalWins = totalWins;
@@ -26,17 +33,18 @@ public class Player {
         this.winRate = winRate;
         this.createdAt = Objects.requireNonNull(createdAt, "Created at cannot be null");
         this.updatedAt = Objects.requireNonNull(updatedAt, "Updated at cannot be null");
+        this.balance = Objects.requireNonNull(balance, "Balance cannot be null");
     }
 
     public static Player createNew(PlayerName name) {
         LocalDateTime now = LocalDateTime.now();
-        return new Player(null, name, 0, 0, 0, 0.0, now, now);
+        return new Player(null, name, 0, 0, 0, 0.0, now, now, BigDecimal.ZERO);
     }
 
 
     public static Player reconstitute(PlayerId id, PlayerName name, int totalWins, int totalLosses,
-                                      int totalGames, double winRate, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        return new Player(id, name, totalWins, totalLosses, totalGames, winRate, createdAt, updatedAt);
+                                      int totalGames, double winRate, LocalDateTime createdAt, LocalDateTime updatedAt, BigDecimal balance) {
+        return new Player(id, name, totalWins, totalLosses, totalGames, winRate, createdAt, updatedAt, balance);
     }
 
     public void recordWin() {
@@ -70,6 +78,21 @@ public class Player {
         if (totalGames > 0) {
             this.winRate = (double) totalWins / totalGames * 100;
         }
+    }
+
+    public void placeBet(PlayerBet bet) {
+        Objects.requireNonNull(bet, "Bet cannot be null");
+        if (this.balance.compareTo(bet.amount()) < 0) {
+            throw new InsufficientBalanceException("Balance is insufficient");
+        }
+        this.balance = this.balance.subtract(bet.amount());
+    }
+
+    public void creditBalance(BigDecimal amount) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
+        }
+        this.balance = this.balance.add(amount);
     }
 
 
@@ -114,5 +137,8 @@ public class Player {
 
     public boolean hasId() {
         return id != null;
+    }
+    public BigDecimal getBalance() {
+        return balance;
     }
 }
