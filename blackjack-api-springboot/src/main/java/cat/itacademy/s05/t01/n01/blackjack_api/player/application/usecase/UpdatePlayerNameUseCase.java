6aac818097a7c5c0291167353b4d06bd5ac08fc7
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-16 12:24:22 +0100

    fix: update class UpdatePlayerNameUseCase to handle dtos

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/application/usecase/UpdatePlayerNameUseCase.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/application/usecase/UpdatePlayerNameUseCase.java
@@ -1,35 +1,37 @@
 package cat.itacademy.s05.t01.n01.blackjack_api.player.application.usecase;
 
+import cat.itacademy.s05.t01.n01.blackjack_api.player.application.dto.ResponsePlayerDTO;
+import cat.itacademy.s05.t01.n01.blackjack_api.player.application.exception.PlayerIdDoesNotExistsInDataBaseException;
 import cat.itacademy.s05.t01.n01.blackjack_api.player.application.exception.PlayerNameAlreadyExistsException;
+import cat.itacademy.s05.t01.n01.blackjack_api.player.application.mapper.PlayerMapper;
 import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.Player;
 
+import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.valueobject.PlayerId;
 import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.model.valueobject.PlayerName;
 import cat.itacademy.s05.t01.n01.blackjack_api.player.domain.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 
 @Service
+@RequiredArgsConstructor
 public class UpdatePlayerNameUseCase {
 
-    @Autowired
     private final PlayerRepository playerRepository;
+    private final PlayerMapper mapper;
 
-    public UpdatePlayerNameUseCase(PlayerRepository playerRepository) {
-        this.playerRepository = playerRepository;
-    }
 
-    public Mono<Player> updatePlayerName(PlayerName name) {
-        return playerRepository.existsByName(name)
-                .flatMap(exists -> {
-                    if (exists) {
-                        return Mono.error(new PlayerNameAlreadyExistsException("Player with name " + name.value() + " already exists."));
-                    } else {
-                        Player newPlayer = Player.createNew(name);
-                        return playerRepository.save(newPlayer);
-                    }
-                });
+    public Mono<ResponsePlayerDTO> execute(PlayerId id, PlayerName name) {
+        return playerRepository.findById(id)
+                .switchIfEmpty(Mono.error(new PlayerIdDoesNotExistsInDataBaseException("Player with id " + id + " does not exists in data base")))
+                .map(player -> {
+                    player.changeName(name);
+                    return player;
+                })
+                .flatMap(playerRepository::save)
+                .map(mapper::toResponse);
     }
 
 }
