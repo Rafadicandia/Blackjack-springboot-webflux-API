79a65e20f1ebcab9a92f1083255ea9e56d215259
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-19 13:25:36 +0100

    update playerEntity
    remove incorrect library for anotations ans implements Persistable<String> for proper management of database flux

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/infrastructure/persistence/mysql/entity/PlayerEntity.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/infrastructure/persistence/mysql/entity/PlayerEntity.java
@@ -1,20 +1,24 @@
 package cat.itacademy.s05.t01.n01.blackjack_api.player.infrastructure.persistence.mysql.entity;
 
-import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
-@Table(schema = "players")
+@Table("players")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PlayerEntity {
+public class PlayerEntity implements Persistable<String> {
 
     @Id
     private String id;
@@ -25,8 +29,25 @@ public class PlayerEntity {
     private Double winRate;
     private Integer totalGames;
     private BigDecimal balance;
-    private String createdAt;
-    private String updatedAt;
-    private String deletedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
 
+    @Transient
+    private boolean isNewRecord = false;
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNewRecord || id == null;
+    }
+
+    public PlayerEntity asNew() {
+        this.isNewRecord = true;
+        return this;
+    }
 }
