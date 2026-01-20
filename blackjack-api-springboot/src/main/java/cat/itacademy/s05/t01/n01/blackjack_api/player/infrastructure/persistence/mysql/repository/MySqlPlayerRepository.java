066d79b312c4434917d55fd231818de0fe1fd75d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-20 11:59:16 +0100

    update MySqlPlayerRepository add repositroy notation

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/infrastructure/persistence/mysql/repository/MySqlPlayerRepository.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/player/infrastructure/persistence/mysql/repository/MySqlPlayerRepository.java
@@ -2,7 +2,8 @@ package cat.itacademy.s05.t01.n01.blackjack_api.player.infrastructure.persistenc
 
 import cat.itacademy.s05.t01.n01.blackjack_api.player.infrastructure.persistence.mysql.entity.PlayerEntity;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface MySqlPlayerRepository extends R2dbcRepository<PlayerEntity, String> {
 }
