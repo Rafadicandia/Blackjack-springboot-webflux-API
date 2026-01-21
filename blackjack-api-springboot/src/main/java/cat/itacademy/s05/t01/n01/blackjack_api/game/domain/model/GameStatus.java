8fffad0a6973e1ff60474e62bb4af5b7f11c5e0d
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-21 20:55:35 +0100

    feat: add GameStatus as enum

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/domain/model/GameStatus.java
@@ -0,0 +1,11 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.domain.model;
+
+public enum GameStatus {
+    IN_PROGRESS,
+    PLAYER_BLACKJACK,
+    PLAYER_WINS,
+    PLAYER_BUSTED,
+    DEALER_WINS,
+    DEALER_BUSTED,
+    TIE
+}
