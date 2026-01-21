c40ea824594748836d71d1a1656d9efe2de59b82
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-21 16:16:10 +0100

    feat: add GameRepository to domain layer

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/domain/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.domain.repository;
+
+import cat.itacademy.s05.t01.n01.blackjack_api.game.domain.model.Game;
+import cat.itacademy.s05.t01.n01.blackjack_api.game.domain.model.valueobject.GameId;
+import reactor.core.publisher.Mono;
+
+public interface GameRepository {
+    Mono<Game> save(Game game);
+    Mono<Game> findById(GameId id);
+}
