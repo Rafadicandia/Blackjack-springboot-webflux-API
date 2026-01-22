18389b7f8605904a1b41d9091505bf832f2fc03a
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-22 11:19:27 +0100

    feat: add GameEntity class to infrastructure layer

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/persistence/mongodb/entity/GameEntity.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.persistence.mongodb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "games")
+public class GameEntity {
+    @Id
+    private String gameId;
+    private String playerId;
+    private String playerName;
+    private HandEntity playerHand;
+    private HandEntity dealerHand;
+    private DeckEntity deck;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+
+}
