46e52f6349f643156c01d02f50bb5bf7439a2a73
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-22 13:36:20 +0100

    feat: add public constructors to Hand class domain in order to be able to map them in infrastructure layer

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/domain/model/Hand.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/domain/model/Hand.java
@@ -3,12 +3,22 @@ package cat.itacademy.s05.t01.n01.blackjack_api.game.domain.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class Hand {
     private final List<Card> cards;
 
     private Hand(List<Card> cards) {
         this.cards = Collections.unmodifiableList(cards);
+    }
+
+    public Hand() {
+        this.cards = new ArrayList<>();
+    }
+
+    public static Hand from(List<Card> cards) {
+        Objects.requireNonNull(cards, "Cards cannot be null");
+        return new Hand(cards);
     }
 
     public static Hand empty() {
@@ -51,7 +61,7 @@ public class Hand {
     public boolean shouldDealerHit() {
         return calculateScore() < 17;
     }
-    
+
     public List<Card> getCards() {
         return cards;
     }
