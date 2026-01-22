52f8e8bacc1533ac1316f87d19c118d80cee7ae2
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-22 11:20:36 +0100

    feat: add MongoDbRepository to infrastructure layer

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/persistence/mongodb/repository/MongoDbRepository.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.persistence.mongodb.repository;
+
+public interface MongoDbRepository extends ReactiveMongoRepository<GameEntity, String> {
+}
