9d4d5b8bb2f80baff8e65b2c747c250ffb385a55
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-22 11:19:59 +0100

    feat: add HandEntity class to infrastructure layer

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/persistence/mongodb/entity/HandEntity.java
@@ -0,0 +1,18 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.persistence.mongodb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HandEntity {
+    private List<CardEntity> cards;
+
+
+}
