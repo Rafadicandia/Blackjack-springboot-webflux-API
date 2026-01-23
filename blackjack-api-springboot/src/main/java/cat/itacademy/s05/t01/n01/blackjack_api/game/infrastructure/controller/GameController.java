1e41b111587fcfa4282624c4126f84f85ca78df0
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-23 18:44:48 +0100

    refactor(game): update game controller logic

--- blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/controller/GameController.java
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/controller/GameController.java
@@ -1,4 +1,4 @@
-package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.http.controller;
+package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.controller;
 
 import cat.itacademy.s05.t01.n01.blackjack_api.game.application.dto.GameResponseDTO;
 import cat.itacademy.s05.t01.n01.blackjack_api.game.application.dto.PlayGameRequestDTO;
