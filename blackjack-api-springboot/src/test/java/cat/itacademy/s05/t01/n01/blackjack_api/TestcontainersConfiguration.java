21f5d026fe3e5678d5f69cae75679caac14b8729
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-23 18:44:38 +0100

    refactor(test): update testcontainers configuration with specific versions
    - Change MongoDBContainer to use mongo:7.0
    - Change MySQLContainer to use mysql:8.0 with init script
    - Update imports to use generic container classes
    - Change class visibility from package-private to public

--- blackjack-api-springboot/src/test/java/cat/itacademy/s05/t01/n01/blackjack_api/TestcontainersConfiguration.java
+++ blackjack-api-springboot/src/test/java/cat/itacademy/s05/t01/n01/blackjack_api/TestcontainersConfiguration.java
@@ -3,23 +3,24 @@ package cat.itacademy.s05.t01.n01.blackjack_api;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.mongodb.MongoDBContainer;
-import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
 	MongoDBContainer mongoDbContainer() {
-		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+		return new MongoDBContainer(DockerImageName.parse("mongo:7.0"));
 	}
 
 	@Bean
 	@ServiceConnection
-	MySQLContainer mysqlContainer() {
-		return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+	MySQLContainer<?> mysqlContainer() {
+		return new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+				.withInitScript("schema.sql");
 	}
 
 }
