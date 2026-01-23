68e4df79ccbe3a420ace1a468a9fbcff8fbe9209
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-01-23 12:43:27 +0100

    Feat: add game controller with post method

---
+++ blackjack-api-springboot/src/main/java/cat/itacademy/s05/t01/n01/blackjack_api/game/infrastructure/http/controller/GameController.java
@@ -0,0 +1,31 @@
+package cat.itacademy.s05.t01.n01.blackjack_api.game.infrastructure.http.controller;
+
+import cat.itacademy.s05.t01.n01.blackjack_api.game.application.dto.GameResponseDTO;
+import cat.itacademy.s05.t01.n01.blackjack_api.game.application.dto.PlayGameRequestDTO;
+import cat.itacademy.s05.t01.n01.blackjack_api.game.application.usecase.CreateGameUseCase;
+import cat.itacademy.s05.t01.n01.blackjack_api.game.application.usecase.GetGameUseCase;
+import cat.itacademy.s05.t01.n01.blackjack_api.game.application.usecase.PlayGameUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/game")
+@RequiredArgsConstructor
+public class GameController {
+
+    private final CreateGameUseCase createGameUseCase;
+    private final GetGameUseCase getGameUseCase;
+    private final PlayGameUseCase playGameUseCase;
+
+    @PostMapping("/{id}/play")
+    public Mono<ResponseEntity<GameResponseDTO>> playGame(
+            @PathVariable String id,
+            @RequestBody PlayGameRequestDTO request
+    ) {
+        return playGameUseCase.execute(id, request)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+}
